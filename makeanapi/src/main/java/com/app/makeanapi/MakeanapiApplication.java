217331d9169ef18beddfffad5a6a5a790904d762
Felipe Carvalho Sena <felipecsena877@gmail.com>
2024-09-18 22:16:55 -0300

    corrigindo meu git

---
+++ makeanapi/src/main/java/com/app/makeanapi/MakeanapiApplication.java
@@ -0,0 +1,13 @@
+package com.app.makeanapi;
+
+import com.app.makeanapi.domain.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeanapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeanapiApplication.class, args);
+	}
+}
